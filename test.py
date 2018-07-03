295081708bf8a2c232c2e1445e38106ad6d64975
altriser <40743602+altriser@users.noreply.github.com>
2018-07-03 20:02:38 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+print "hello world"
+x = "test"
+y = 3
+r = "W12312"
+kennzeichen = "W123123123"
+
+t = "gruen"
+farbe = "gruen"
+print farbe
+print x, y
+u = 3.2
+print u
+d = True
+e = False
+print d,e
